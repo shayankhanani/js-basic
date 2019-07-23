85489a7c27ada730baa43c08c2c029fb021c4ed6
shayankhanani <shayan.khanani@outlook.com>
2019-07-23 23:34:12 +0500

    Conditional Statements are used to perform different actions for different decisions
    Use if to specify a block of code to be executed, if a specified
    condition is true
    Use else to specify a block of code to be executed, if the same
    condition is false

---
+++ conditional.js
@@ -0,0 +1,17 @@
+function ifState(){
+    var x = prompt("What is 2 + 2?");
+    //if check condition and execute if condition is true
+    if (x === "4") {
+        alert("Correct!");
+    }
+}
+function elseState(){
+    var x = prompt("What is 2 + 2?");
+    //if check condition and execute if condition is true
+    if (x === 4) {
+        alert("Correct!");
+    }
+    else{
+        alert("Dtat type mismatch");
+    }
+}
