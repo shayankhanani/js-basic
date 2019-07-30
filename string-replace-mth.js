8740af35f1e6617f39ffbbb1a77f13dd24930eef
shayankhanani <shayan.khanani@outlook.com>
2019-07-30 19:24:42 +0500

    The replace method in js help to replace part of a string. replace(the part to replace, what to replace with) is general overview of replace method.
    Note: replace method will only replace first instance of a string to
    replace all instances global replace will be there by adding closing
    slash with g replace(/the part to replace/g, what to replace with) like
    this

---
+++ string-replace-mth.js
@@ -0,0 +1,8 @@
+var text = "It is startling to think that, even in the darkest depths \
+of World War II, J. R. R. Tolkien was writing the trilogy, which contains, \
+with the weird applicability available only to poetry and myth, the essential \
+notion that the good gray wizard can understand the evil magi preciselybecause \
+he is just enough like them to grasp their minds and motives in ways that they \
+cannot grasp his.";
+
+text = text.replace("World War II","the Second World War");
