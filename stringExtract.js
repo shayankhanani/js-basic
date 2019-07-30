10c38d1d735c5236a1088288292788f3cd10d918
shayankhanani <shayan.khanani@outlook.com>
2019-07-30 18:12:25 +0500

    Changes in Alert Message Box - Concatination

--- stringExtract.js
+++ stringExtract.js
@@ -1,4 +1,4 @@
 var input = prompt("Enter your Last Name",'Lee');
-alert("Welcome " + "Mr."+input);
+alert("Welcome " + "Mr. "+input);
 var initials = input.slice(0,4).toUpperCase();
 alert("Your initials are: "+ initials);
