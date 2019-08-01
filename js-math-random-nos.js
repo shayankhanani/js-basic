112559c6c3f7d37be4ac8b30b8a17ca75a835211
shayankhanani <shayan.khanani@outlook.com>
2019-08-01 23:24:35 +0500

    random function: always delivers a 16-place decimal that ranges from 0.0000000000000000 to 0.9999999999999999.
    we can multiply the generated number with integer to get larger number
    and then we can round up or down to get perfect integer.

---
+++ js-math-random-nos.js
@@ -0,0 +1,9 @@
+var randomNo = Math.random();
+console.log(randomNo);
+var randomNo2 = randomNo * 100000000000000000;
+console.log(randomNo2);
+
+var randomDecimal = Math.random();
+var improved = (randomDecimal * 6) + 1;
+var numberOfStars = Math.floor(improved);
+console.log(numberOfStars);
