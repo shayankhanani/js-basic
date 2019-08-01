641d33b5babe15398059188d24297de01a4f831f
shayankhanani <shayan.khanani@outlook.com>
2019-08-01 23:55:09 +0500

    Number method: to convert string integer or floating number into actual integer or floating number toString Method: to convert any number to string.

---
+++ js-conv-int-string.js
@@ -0,0 +1,11 @@
+var intString = "24"
+var num = Number(intString);
+console.log(num);
+
+var floatNumString = "24.9876";
+var num = Number(floatNumString);
+console.log(num);
+
+var numAsNum = 1234;
+var numbAsStr = numAsNum.toString();
+console.log(numAsNum + "3214");
