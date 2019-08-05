e40e8b8e9b074efc3c0a1b3e6d0fa22e67fa2119
shayankhanani <shayan.khanani@outlook.com>
2019-08-05 23:14:04 +0500

    To get different parts of dates, there are several methods for extracting parts of date object
    getDay() - gets day of week from 0 to 6
    getMonth() - gets months from 0 to 11
    getDate() - gets date of month from 1 to 31
    getFullYear() - get the year of date object
    getHours() - gets hour of day from 0 to 23
    getMinutes() - gets minutes from 0 to 59
    getSeconds() - gets seconds from 0 to 59
    getMilliseconds() - gets milliseconds from 0 to 999
    getTime() - milliseconds since midnight January 1, 1970

---
+++ date-extract-parts.js
@@ -0,0 +1,10 @@
+var d = new Date();
+console.log(d.getDay());
+console.log(d.getMonth());
+console.log(d.getDate());
+console.log(d.getFullYear());
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
+console.log(d.getMilliseconds());
+console.log(d.getTime());
