32867186abef5db979b8f3d733baf2feb1fccbaa
shayankhanani <shayan.khanani@outlook.com>
2019-08-10 22:13:59 +0500

    to set individual part of date js allow following methods setFullYear() : set the specified year setMonth() : set the specified month 1 to 12 setDate() : set the specified month 1 to 31 setHours() : set the specified hour 1 to 24 setMinutes() : set the specified minutes setSeconds() : set the specified seconds setMilliseconds() : set the specified milliseconds

---
+++ date-set-parts.js
@@ -0,0 +1,12 @@
+var myDate = new Date();
+console.log(myDate);
+myDate.setFullYear(2019);
+console.log(myDate);
+myDate.setDate(11);
+console.log(myDate);
+myDate.setMonth(9);
+console.log(myDate);
+myDate.setHours(6);
+console.log(myDate);
+myDate.setMinutes(12);
+console.log(myDate);
