d9b62fe890dcaa04dc5f5baf3644ba1999a06793
shayankhanani <shayan.khanani@outlook.com>
2019-09-24 21:40:25 +0500

    Objects in JavaScript may be defined as an unordered collection of  related data, of primitive or reference types, in the form of  “key: value” pairs.
     objects are created in similar fashion like arrays but begins and end
     with { }

---
+++ object-js/intro-objects.js
@@ -0,0 +1,17 @@
+var city1 = {
+    id : 0,
+    name : 'Karachi',
+    status : 'Provincial Capital',
+    state : 'sindh' 
+};
+
+console.log(city1);
+
+city1.population = '16.62M';
+
+console.log(city1);
+
+city1.ditstricts = ['east','west','north','south','central'];
+
+console.log(city1)
+console.log(city1.ditstricts[4])
